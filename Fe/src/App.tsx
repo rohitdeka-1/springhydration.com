6311b14bb6cbd264f31b387533e49e4931720eed
Rohit Deka <rohit.23MIM10093@vitbhopal.ac.in>
2026-08-15 18:30:01 +0530

    feat: scaffold initial frontend project with basic app structure and global styles

--- Fe/src/App.tsx
+++ Fe/src/App.tsx
@@ -2,15 +2,11 @@ import { useEffect, useState } from "react"
 
 const COLORS = [
   "bg-white",
-  "bg-blue-400",
-  "bg-green-400",
-  "bg-yellow-300",
-  "bg-orange-400",
-  "bg-pink-400",
-  "bg-purple-400",
-  "bg-red-400",
   "bg-blue-500",
   "bg-red-500",
+  "bg-green-500",
+  "bg-yellow-500",
+  "bg-purple-500",
 ];
 
 function App() {
@@ -27,15 +23,15 @@ function App() {
     <>
       <div className={`text-3xl flex flex-col min-h-screen relative ${COLORS[colorIndex]} w-full items-center justify-center transition-colors duration-1000 ease-in-out`}>
         <div className="flex flex-col px-4">
-          <h1 className="text-black text-left text-[95px] sm:text-[120px] md:text-[160px] lg:text-[200px] leading-none tracking-[0.093em]">SPRING</h1>
-          <h1 className="text-black text-left text-[36px] sm:text-[42px] md:text-[56px] lg:text-[70px] leading-none tracking-[0.091em] ml-1 sm:ml-2 lg:ml-3">HYDRATION</h1>
+          <h1 className={`text-${COLORS[colorIndex] == "bg-white" ? "black" : "white"} ease-in text-left text-[120px] sm:text-[120px] md:text-[160px] lg:text-[200px] leading-none tracking-[0.06em]`}>SPRING</h1>
+          <h2 className={`text-${COLORS[colorIndex] == "bg-white" ? "black" : "white"} ease-in text-center text-[26px]  sm:text-[42px] md:text-[42px] lg:text-[29px] leading-none tracking-[0.091em]  sm:ml-2 lg:ml-3`}>HYDRATION</h2>
         </div>
 
         <div className="absolute bottom-0 w-full py-4">
-          <p className="text-black text-center text-sm tracking-widest font-semibold">LAUNCHING SOON....</p>
+          <p className={`text-${COLORS[colorIndex] == "bg-white" ? "black" : "white"} ease-in-out text-center text-sm tracking-widest `}>LAUNCHING SOON....</p>
         </div>
 
-      </div>
+      </div >
     </>
   )
 }
