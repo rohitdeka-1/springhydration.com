d54aca51cb6600760fdb683b2440aad9d56d3c11
Rohit Deka <rohit.23MIM10093@vitbhopal.ac.in>
2026-08-15 18:33:39 +0530

    feat: initialize application with a rotating color background landing page

--- Fe/src/App.tsx
+++ Fe/src/App.tsx
@@ -23,12 +23,12 @@ function App() {
     <>
       <div className={`text-3xl flex flex-col min-h-screen relative ${COLORS[colorIndex]} w-full items-center justify-center transition-colors duration-1000 ease-in-out`}>
         <div className="flex flex-col px-4">
-          <h1 className={`text-${COLORS[colorIndex] == "bg-white" ? "black" : "white"} ease-in text-left text-[120px] sm:text-[120px] md:text-[160px] lg:text-[200px] leading-none tracking-[0.06em]`}>SPRING</h1>
-          <h2 className={`text-${COLORS[colorIndex] == "bg-white" ? "black" : "white"} ease-in text-center text-[26px]  sm:text-[42px] md:text-[42px] lg:text-[29px] leading-none tracking-[0.091em]  sm:ml-2 lg:ml-3`}>HYDRATION</h2>
+          <h1 className={`${COLORS[colorIndex] == "bg-white" ? "text-black" : "text-white"} ease-in text-left text-[120px] sm:text-[120px] md:text-[160px] lg:text-[200px] leading-none tracking-[0.06em]`}>SPRING</h1>
+          <h2 className={`${COLORS[colorIndex] == "bg-white" ? "text-black" : "text-white"} ease-in text-center text-[26px]  sm:text-[42px] md:text-[42px] lg:text-[29px] leading-none tracking-[0.091em]  sm:ml-2 lg:ml-3`}>HYDRATION</h2>
         </div>
 
         <div className="absolute bottom-0 w-full py-4">
-          <p className={`text-${COLORS[colorIndex] == "bg-white" ? "black" : "white"} ease-in-out text-center text-sm tracking-widest `}>LAUNCHING SOON....</p>
+          <p className={`${COLORS[colorIndex] == "bg-white" ? "text-black" : "text-white"} ease-in-out text-center text-sm tracking-widest `}>LAUNCHING SOON....</p>
         </div>
 
       </div >
