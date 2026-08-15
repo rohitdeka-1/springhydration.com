2297572dab81c34acbfafb191948ad782cf93ffa
Rohit Deka <rohit.23MIM10093@vitbhopal.ac.in>
2026-08-15 14:29:32 +0530

    first

--- Fe/src/App.tsx
+++ Fe/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 
 const COLORS = [
+  "bg-white",
   "bg-blue-400",
   "bg-green-400",
   "bg-yellow-300",
@@ -9,19 +10,13 @@ const COLORS = [
   "bg-purple-400",
   "bg-red-400",
   "bg-blue-500",
-  "bg-green-500",
-  "bg-yellow-400",
   "bg-orange-500",
   "bg-pink-500",
   "bg-purple-500",
   "bg-red-500",
   "bg-blue-600",
-  "bg-green-600",
   "bg-yellow-500",
-  "bg-orange-600",
-  "bg-pink-600",
-  "bg-purple-600",
-  "bg-red-600"
+  "bg-orange-600"
 ];
 
 function App() {
